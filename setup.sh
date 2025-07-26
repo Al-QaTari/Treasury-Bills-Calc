f2cf78c1b3a5bef601b0b81b30a0f42f96957015
MoOo <49832380+Al-QaTari@users.noreply.github.com>
2025-07-26 06:52:48 +0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-playwright install chromium
+# Make the directory for playwright browsers and change ownership
+mkdir -p /home/appuser/.cache/ms-playwright
+chown -R appuser:appuser /home/appuser/.cache/ms-playwright
+
+# Run the playwright install command as the appuser
+sudo -u appuser python -m playwright install chromium
